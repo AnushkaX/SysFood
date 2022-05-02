347d6683191608d3d9526ed97607af74c7db5496
AnushkaX <anushka.mutusinghe@gmail.com>
2022-05-02 22:15:41 +0530

    models updated

---
+++ frontend/src/homeComponents/ContactInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ContactInfo = () => {
+    return (
+        <div className="contactInfo container">
+            <div className="row">
+                <div className="col-12 col-md-4 contact-Box">
+                    <div className="box-info">
+                        <div className="info-image">
+                            <i className="fas fa-phone-alt"></i> 
+                        </div>
+                        <h5>Call us on 0112345678</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactInfo;

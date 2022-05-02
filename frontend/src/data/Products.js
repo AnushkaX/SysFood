419f1dcc2e4141e19179c71ad462c17429c35e95
AnushkaX <anushka.mutusinghe@gmail.com>
2022-05-02 22:22:27 +0530

    bootstrap added

--- frontend/src/data/Products.js
+++ frontend/src/data/Products.js
@@ -26,6 +26,60 @@ const products = [
         description: "Lorem Ipsom",
         price: 89,
     },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },{
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
+    {
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },{
+        _id: "1",
+        name: "Dummy 1",
+        image: "",
+        description: "Lorem Ipsom",
+        price: 89,
+    },
 ];
 
 export default products;
